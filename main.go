9a76e936dfa75372ecb8ed968e2db4b71a71395e
mjmhtjain <42150844+mjmhtjain@users.noreply.github.com>
2021-06-01 20:11:48 +0530

    - integration of router with server

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/mjmhtjain/go-dynamo/src/router"
 )
 
 func main() {
-	handler := http.HandlerFunc(pingFunc)
-	log.Fatal(http.ListenAndServe(":80", handler))
+	// handler := http.HandlerFunc(pingFunc)
+	r := router.Router()
+	log.Fatal(http.ListenAndServe(":80", r))
 }
 
 func pingFunc(w http.ResponseWriter, r *http.Request) {
