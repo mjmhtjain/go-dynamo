08eef5122e8a58cb5a57c0d7885b966fa1bfa7fa
mjmhtjain <42150844+mjmhtjain@users.noreply.github.com>
2021-06-05 17:59:47 +0530

    - routing based on Http.Method

--- src/handler/userHandler.go
+++ src/handler/userHandler.go
@@ -1,19 +1,35 @@
 package handler
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/mjmhtjain/go-dynamo/src/customError"
+	"github.com/mjmhtjain/go-dynamo/src/model"
 	"github.com/mjmhtjain/go-dynamo/src/repo"
 )
 
 var userDetailRepo repo.UserDetailRepo
 
 func UserHandler(w http.ResponseWriter, r *http.Request) {
-	userId := r.URL.Path[len("/user/"):]
+
+	switch r.Method {
+	case http.MethodGet:
+		findUser(w, r)
+	case http.MethodPost:
+		saveUser(w, r)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+}
+
+func findUser(w http.ResponseWriter, r *http.Request) {
+	userId := strings.TrimPrefix(r.URL.Path, "/user/")
 
 	log.Printf("userId: %v", userId)
 
@@ -38,4 +54,24 @@ func UserHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, resp)
+}
+
+func saveUser(w http.ResponseWriter, r *http.Request) {
+	user := new(model.UserDetail)
+	json.NewDecoder(r.Body).Decode(&user)
+
+	log.Printf("user: %v", user)
+
+	if userDetailRepo == nil {
+		log.Println("NewUserDetailRepo created .. ")
+		userDetailRepo = repo.NewUserDetailRepo()
+	}
+	err := userDetailRepo.Save(user)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
